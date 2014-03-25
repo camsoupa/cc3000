6c60799aca5e514cd1e6681a1b0289edbe53c286
cam <camsoupa@gmail.com>
2014-03-25 14:07:21 -0600

    adding M.P.'s initial projects

---
+++ cc3000fpga/SoftConsole/cc3000fpga_MSS_MSS_CM3_0/cc3000fpga_MSS_MSS_CM3_0_hw_platform/cc3000fpga_hw_platform.h
@@ -0,0 +1,16 @@
+#ifndef cc3000fpga_HW_PLATFORM_H_
+#define cc3000fpga_HW_PLATFORM_H_
+/*****************************************************************************
+*
+*Created by Actel SmartDesign  Mon Mar 24 21:56:16 2014
+*
+*Memory map specification for peripherals in cc3000fpga
+*/
+
+/*-----------------------------------------------------------------------------
+* MSS_CM3_0 subsystem memory map
+* Master(s) for this subsystem: MSS_CM3_0 
+*---------------------------------------------------------------------------*/
+
+
+#endif /* cc3000fpga_HW_PLATFORM_H_*/
---
+++ cc3000fpga/synthesis/cc3000fpga_syn.tcl
@@ -0,0 +1,3 @@
+project -load "C:/Users/Gnewt/Desktop/cc3000fpga/synthesis/cc3000fpga_syn.prj"
+project -run -clean
+project -save

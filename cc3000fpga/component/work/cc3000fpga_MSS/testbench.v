42e32d7b4253cd7a984ed8b997ce4be0a152658f
unknown <mapalmer@DIG-3.users.coe.utah.edu>
2014-04-03 14:02:11 -0600

    adding

---
+++ cc3000fpga/component/work/cc3000fpga_MSS/testbench.v
@@ -0,0 +1,81 @@
+//////////////////////////////////////////////////////////////////////
+// Created by Actel SmartDesign Thu Apr 03 14:00:38 2014
+// Testbench Template
+// This is a basic testbench that instantiates your design with basic 
+// clock and reset pins connected.  If your design has special
+// clock/reset or testbench driver requirements then you should 
+// copy this file and modify it. 
+//////////////////////////////////////////////////////////////////////
+
+`timescale 1ns/100ps
+
+module testbench;
+
+parameter SYSCLK_PERIOD = 100; // 10MHz
+
+reg SYSCLK;
+reg NSYSRESET;
+
+initial
+begin
+    SYSCLK = 1'b0;
+    NSYSRESET = 1'b0;
+end
+
+//////////////////////////////////////////////////////////////////////
+// Reset Pulse
+//////////////////////////////////////////////////////////////////////
+initial
+begin
+    #(SYSCLK_PERIOD * 10 )
+        NSYSRESET = 1'b1;
+end
+
+
+//////////////////////////////////////////////////////////////////////
+// 10MHz Clock Driver
+//////////////////////////////////////////////////////////////////////
+always @(SYSCLK)
+    #(SYSCLK_PERIOD / 2.0) SYSCLK <= !SYSCLK;
+
+
+//////////////////////////////////////////////////////////////////////
+// Instantiate Unit Under Test:  cc3000fpga_MSS
+//////////////////////////////////////////////////////////////////////
+cc3000fpga_MSS cc3000fpga_MSS_0 (
+    // Inputs
+    .MSSPREADY({1{1'b0}}),
+    .MSSPSLVERR({1{1'b0}}),
+    .FABINT({1{1'b0}}),
+    .F2M_GPI_31({1{1'b0}}),
+    .F2M_GPI_30({1{1'b0}}),
+    .F2M_GPI_29({1{1'b0}}),
+    .IO_4_D({1{1'b0}}),
+    .MSSPRDATA({32{1'b0}}),
+    .UART_0_RXD({1{1'b0}}),
+    .UART_1_RXD({1{1'b0}}),
+    .MSS_RESET_N(NSYSRESET),
+    .IO_2_PADIN({1{1'b0}}),
+
+    // Outputs
+    .M2F_GPO_1( ),
+    .M2F_GPO_0( ),
+    .FAB_CLK( ),
+    .M2F_RESET_N( ),
+    .MSSPSEL( ),
+    .MSSPENABLE( ),
+    .MSSPWRITE( ),
+    .M2F_GPO_3( ),
+    .IO_2_Y( ),
+    .MSSPADDR( ),
+    .MSSPWDATA( ),
+    .UART_0_TXD( ),
+    .UART_1_TXD( ),
+    .IO_4_PADOUT( )
+
+    // Inouts
+
+);
+
+endmodule
+
